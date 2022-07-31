f164bd6cd3503365665fa12ca7407b5fe9e84b09
HigorJT <higordejesus2000@gmail.com>
2022-07-30 23:53:52 -0300

    Arquivo em Python

---
+++ Arquivo em Python/Arquivo.py
@@ -0,0 +1,27 @@
+from test import menu, cabeçalho, leiaInt, linha
+def arquivoExiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileExistsError:
+        return False
+    else:
+        return True
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Houve um ERRO na criação de arquivo!')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+        
+    except:
+        print('Erro ao ler o arquivo!')
+    else:
+        cabeçalho('PESSOAS CADASTRADAS')
+        print(a.read())
